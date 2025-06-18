6efcd8193e62d13a4e62cda0ce54d6770260c680
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-18 17:29:31 +0800

    kernel: ksud, core_hook: migrate ksud execution to security_bprm_check
    This migrates ksud execution decision-making to bprm_check_security.
    This requires passing proper argv and envp to a modified _ksud handler
    aptly named 'ksu_handle_bprm_ksud'.
    
    Introduces:
    int ksu_handle_bprm_ksud(const char *filename, const char *argv1,
    				const char *envp_hex)
    
    (insert meme: oh wait, its all char?, always have been)
    
    which is adapted from:
    int ksu_handle_execveat_ksud(int *fd, struct filename **filename_ptr,
    			     struct user_arg_ptr *argv,
    			     struct user_arg_ptr *envp,
    			     int *flags)
    
    ksu_handle_bprm_ksud handles all the decision making, it decides when it is
    time to apply_kernelsu_rules depending if it sees "second_stage".
    
    For LSM hook, turns out we can pull out argv and envp from mm_struct.
    The code in here explains itself on how to do it.
    
    whole blob exists on arg_start to arg_end, so we just pull it out and grab next
    array after the first null terminator.
    
    as for envp, we pack it to hex and pass it to ksu_handle_bprm_ksud so it can strstr
    the whole blob.
    
    as for hardening:
    - proper checks (filtering, oob checks)
    - GFP_ATOMIC kmalloc to avoid or lower blocking time
    - spinlock protected copy_from_user_nofault
    
    My reasoning on protecting that usercopy is that on some devices,
    an unprotected nofault copy does NOT work well. While we can use
    copy_from_user, having proper work to keep atomicity is a good thing.
    https://github.com/backslashxx/KernelSU/issues/21
    
    This change also provides an inlined copy_from_user_nofault for < 5.8.
    Reasonig behind this is that the old probe_user_write for 5.7 and below
    is NOT really _nofault as it doesnt have that pagefault_disable + enable
    which is the hallmark of these _nofault functions.
    
    While using strncpy_from_user_nofault was considered, this wont do, this will
    only copy up to the first \0.
    
    devlog:
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/compare/16e5dce9e7e...16c1f5f521a
    https://github.com/backslashxx/mojito_krenol/compare/28642e60d7b...728de0c571c
    
    References:
    https://elixir.bootlin.com/linux/v4.14.1/source/include/linux/mm_types.h#L429
    https://elixir.bootlin.com/linux/v4.14.1/source/include/linux/lsm_hooks.h
    --
    
    With this change, theres no need to use execve_ksud and execveat_ksud
    handlers anymore.
    
    On next commit, we clean or lock those up.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -23,5 +23,6 @@ extern ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
 				       size_t count, loff_t *pos);
 
 extern int ksu_access_ok(const void *addr, unsigned long size);
+extern long ksu_copy_from_user_nofault(void *dst, const void __user *src, size_t size);
 
 #endif
